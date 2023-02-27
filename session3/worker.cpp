6d2c88981ca4a6ae08eab0730b3b2d4d69d7ada9
Nguyễn Ninh <nguyenvanninh2004pt@gmail.com>
2023-02-27 18:36:32 +0700

    second commit

--- session3/worker.cpp
+++ session3/worker.cpp
@@ -7,7 +7,7 @@ int main() {
     cout << "Nhap ho va ten cua cong nhan : ";
     getline(cin,name);
     cout << "Nhap so ngay cong ho lam trong 1 thang : ";
-    int worday;
+    int workday;
     cin >> workday;cin.ignore();
     cout << "Nhap luong cua moi ngay cong : ";
     int salary;
@@ -15,6 +15,6 @@ int main() {
     cout << "Nhap so tien da ung trong thang : ";
     int advance_salary;
     cin >> advance_salary;
-    cout << "So tien duoc nhan con lai trong thang la :" << (worday*salary-advance_salary) << endl;
+    cout << "So tien duoc nhan con lai trong thang la :" << (workday*salary-advance_salary) << endl;
     return 0;
 }
